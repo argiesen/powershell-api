f0cb9691e1608908a9c70e7f6d1b4e774d5355be
Andy Giesen <argiesen@gmail.com>
2024-09-29 19:50:39 -0700

    Create logmein-get-user-mfa-status.ps1

---
+++ LogMeIn/logmein-get-user-mfa-status.ps1
@@ -0,0 +1,46 @@
+# LogMeIn API
+# Created by: Andy Giesen
+# Date: 2024.09.29
+
+[cmdletbinding()]
+param(
+    [Parameter(Mandatory)]
+    [string]$CompanyId,
+    [Parameter(Mandatory)]
+    [string]$ApiPsk
+)
+
+# Update certificate trust policy to allow untrusted certificates
+add-type @"
+    using System.Net;
+    using System.Security.Cryptography.X509Certificates;
+    public class TrustAllCertsPolicy : ICertificatePolicy {
+        public bool CheckValidationResult(
+            ServicePoint srvPoint, X509Certificate certificate,
+            WebRequest request, int certificateProblem) {
+            return true;
+        }
+    }
+"@
+[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+
+# Useful API documentation: https://developer.goto.com/pdf/Central_Developer_Guide.pdf
+# Validate authentication with CompanyID and API PSK
+$authBase64 = [Convert]::ToBase64String($CompanyId + ":" + $ApiPsk)
+
+$url = "https://secure.logmein.com/public-api/v1/authentication"
+$authResponse = Invoke-WebRequest -Uri $url -Method Get -ContentType "application/json" -UseBasicParsing -Headers @{Authorization="Basic $authBase64"}
+
+$authResponseJson = ($authResponse).Content | ConvertFrom-Json
+
+if ($authResponseJson.success -ne "true"){
+    Write-Host "Authentication failed" -ForegroundColor Red
+    break;
+}
+
+# Get users list
+$url = "https://secure.logmein.com/public-api/v3/users"
+$usersResponse = Invoke-WebRequest -Uri $url -Method Get -ContentType "application/json" -UseBasicParsing -Headers @{Authorization="Basic $authBase64"}
+
+$usersResponseJson = ($usersResponse).Content | ConvertFrom-Json
+
