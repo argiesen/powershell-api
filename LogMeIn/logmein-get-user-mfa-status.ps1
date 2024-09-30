a417337b0ed7d62f48d5f648ead2f989da7974fa
Andy Giesen <argiesen@gmail.com>
2024-09-29 19:57:04 -0700

    Update logmein-get-user-mfa-status.ps1

--- LogMeIn/logmein-get-user-mfa-status.ps1
+++ LogMeIn/logmein-get-user-mfa-status.ps1
@@ -25,22 +25,32 @@ add-type @"
 [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
 
 # Useful API documentation: https://developer.goto.com/pdf/Central_Developer_Guide.pdf
+
 # Validate authentication with CompanyID and API PSK
 $authBase64 = [Convert]::ToBase64String($CompanyId + ":" + $ApiPsk)
 
 $url = "https://secure.logmein.com/public-api/v1/authentication"
-$authResponse = Invoke-WebRequest -Uri $url -Method Get -ContentType "application/json" -UseBasicParsing -Headers @{Authorization="Basic $authBase64"}
+try {
+    $error.Clear()
+    $authResponse = Invoke-WebRequest -Uri $url -Method Get -ContentType "application/json" -UseBasicParsing -Headers @{Authorization="Basic $authBase64"}
+    $authResponseJson = ($authResponse).Content | ConvertFrom-Json
 
-$authResponseJson = ($authResponse).Content | ConvertFrom-Json
-
-if ($authResponseJson.success -ne "true"){
-    Write-Host "Authentication failed" -ForegroundColor Red
+    if ($authResponseJson.success -ne "true"){
+        Write-Host "Authentication failed" -ForegroundColor Red
+        break;
+    }
+}catch{
+    Write-Host "Error making request (URL: $url): $($Error.Exception.Message)" -ForegroundColor Red
     break;
 }
 
 # Get users list
 $url = "https://secure.logmein.com/public-api/v3/users"
-$usersResponse = Invoke-WebRequest -Uri $url -Method Get -ContentType "application/json" -UseBasicParsing -Headers @{Authorization="Basic $authBase64"}
-
-$usersResponseJson = ($usersResponse).Content | ConvertFrom-Json
-
+try {
+    $error.Clear()
+    $usersResponse = Invoke-WebRequest -Uri $url -Method Get -ContentType "application/json" -UseBasicParsing -Headers @{Authorization="Basic $authBase64"}
+    $usersResponseJson = ($usersResponse).Content | ConvertFrom-Json
+}catch{
+    Write-Host "Error making request (URL: $url): $($Error.Exception.Message)" -ForegroundColor Red
+    break;
+}
