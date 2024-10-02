1874ffded994d949c531f8829b0843c9a5e7a0c8
Andy Giesen <argiesen@gmail.com>
2024-10-01 23:11:08 -0700

    Update logmein-get-user-mfa-status.ps1

--- LogMeIn/logmein-get-user-mfa-status.ps1
+++ LogMeIn/logmein-get-user-mfa-status.ps1
@@ -1,6 +1,9 @@
 # LogMeIn API
-# Created by: Andy Giesen
-# Date: 2024.09.29
+# Created by: CompuNet Inc
+# Authors: Andy Giesen <agiesen@compunet.biz>
+# Last Modified October 1, 2024
+#
+#
 
 [cmdletbinding()]
 param(
@@ -11,15 +14,19 @@ param(
 )
 
 # Useful API documentation: https://developer.goto.com/pdf/Central_Developer_Guide.pdf
+# https://developer.goto.com/admin/
 
 # Validate authentication with CompanyID and API PSK
-$authString = [System.Text.Encoding]::Unicode.GetBytes($CompanyId + ":" + $ApiPsk)
-$authBase64 = [Convert]::ToBase64String($authString)
+$authString = $CompanyId + ":" + $ApiPsk
+$authBase64 = [Convert]::ToBase64String([char[]]$authString)
 
 $url = "https://secure.logmein.com/public-api/v1/authentication"
+$authResponse = Invoke-WebRequest -Uri $url -Method Get -UseBasicParsing -Headers @{Authorization="Basic $authBase64"}
+$authResponse
+
 try {
     $error.Clear()
-    $authResponse = Invoke-WebRequest -Uri $url -Method Get -ContentType "application/json" -UseBasicParsing -Headers @{Authorization="Basic $authBase64"}
+    $authResponse = Invoke-WebRequest -Uri $url -Method Get -UseBasicParsing -Headers @{Authorization="Basic $authBase64"}
     $authResponseJson = ($authResponse).Content | ConvertFrom-Json
 
     if ($authResponseJson.success -ne "true"){
@@ -27,7 +34,7 @@ try {
         return;
     }
 }catch{
-    Write-Host "Error making request (URL: $url): $($Error.Exception.Message)" -ForegroundColor Red
+    Write-Host "Error making authentication request (URL: $url): $($Error.Exception.Message)" -ForegroundColor Red
     return;
 }
 
@@ -36,10 +43,11 @@ $url = "https://secure.logmein.com/public-api/v3/users"
 try {
     $error.Clear()
     $usersResponse = Invoke-WebRequest -Uri $url -Method Get -ContentType "application/json" -UseBasicParsing -Headers @{Authorization="Basic $authBase64"}
+    $usersResponse.Content
     $usersResponseJson = ($usersResponse).Content | ConvertFrom-Json
 }catch{
     Write-Host "Error making request (URL: $url): $($Error.Exception.Message)" -ForegroundColor Red
     return;
 }
 
-$usersResponseJson | Format-Table id,email,firstName,lastName,lastLoginDate
+$usersResponseJson.usersData | Format-Table id,isPending,isEnabled,email,firstName,lastName,lastLoginDate
